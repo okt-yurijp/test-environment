9152f6737f5ac65c5b286f1c3c7f23ce10e18256
Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
2025-03-16 16:27:25 +0400

    selftest/minimal: add test for multi-line indent stripping
    Signed-off-by: Yurij Plotnikov <yurij.plotnikov@arknetworks.am>
    Reviewed-by: Denis Pryazhennikov <denis.pryazhennikov@arknetworks.am>

---
+++ suites/selftest/ts/minimal/multi_line_indent.c
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2025 OKTET Ltd. All rights reserved. */
+/** @file
+ * @brief Minimal test
+ *
+ * Minial test scenario that checks indentation stripping.
+ */
+
+/** @page minimal_multi_line_indent Multi-line indentation test
+ *
+ * @objective Check that indentation of multi-line is stripped correctly by TE.
+ *
+ * @par Test sequence:
+ *
+ */
+
+#ifndef DOXYGEN_TEST_SPEC
+
+/** Logging subsystem entity name */
+#define TE_TEST_NAME    "multi_line_indent"
+
+#include "te_config.h"
+#include "tapi_test.h"
+
+int
+main(int argc, char **argv)
+{
+    const char *test_line;
+    TEST_START;
+
+    TEST_GET_STRING_PARAM(test_line);
+
+    TEST_STEP("Print the value of 'test_line' parameter");
+    RING("Value of 'test_line11' param is: %s", test_line);
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    TEST_END;
+}
+
+#endif /* !DOXYGEN_TEST_SPEC */
